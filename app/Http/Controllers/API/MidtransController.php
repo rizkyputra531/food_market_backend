3bc80c1f919c853ed1482744972c3e0841625248
Muhammad Rizky Syahputra <rizkyputra531@Rizky-Macbook.local>
2021-12-09 11:55:19 +0800

    Notification Message

--- app/Http/Controllers/API/MidtransController.php
+++ app/Http/Controllers/API/MidtransController.php
@@ -51,11 +51,24 @@ class MidtransController extends Controller
         } else if ($status == 'cancel') {
             $transaction->status = 'CANCELLED';
         }
-
-
-
-
         //simpan transaksi
         $transaction->save();
     }
+
+    public function success()
+    {
+        return view('midtrans.success');
+    }
+
+    public function unfinish()
+    {
+        return view('midtrans.unfinish');
+    }
+
+    public function error()
+    {
+        return view('midtrans.error');
+    }
+
+
 }
