88718c2204eff0e1fb2eb6bceed6b7aa676d2d02
Muhammad Rizky Syahputra <rizkyputra531@Rizky-Macbook.local>
2021-12-24 13:49:29 +0800

    User Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,7 +27,8 @@ Route::get('/', function () {
 Route::prefix('dashboard')
     ->middleware(['auth:sanctum', 'admin'])
     ->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('users', UserController::class);
     });
 
 // Route::get('/debug-sentry', function () {
